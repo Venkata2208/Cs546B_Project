a164242d2b6067617abfd88cd004269d84bc1391
Hari Obili <obiliharireddy@gmail.com>
2022-12-12 01:58:22 -0500

    edit a match backend

--- routes/matches.js
+++ routes/matches.js
@@ -9,6 +9,16 @@ router.post("/", matches.createMatch);
 router.get("/history", matches.getMatches);
 router.get("/highlights", matches.getHighlights);
 router.post("/highlights", matches.postHighlights);
+// router.get("/scorecared", matches.getScoreCared);
+router.post("/scorecared", matches.postscorecard);
+
+// router.post("/highlights", matches.postHighlights);
+// router.route("/highlights").post(async (req, res) => {
+//   let matchID = req.body.matchID;
+//   let highlight = req.body.highlight;
+//   matches.postHighlights2(matchID, highlight);
+//   res.redirect("/matches/history");
+// });
 
 // router.put("/", matches.updateMatch);
 // router.post("/comments", matches.postComment);
