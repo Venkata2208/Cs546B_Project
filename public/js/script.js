294452d20478346d36c6143966db8d8387d4c04e
Venkata2208 <vjammigu@stevens.edu>
2022-12-11 19:02:54 -0500

    commit2byvn

--- public/js/script.js
+++ public/js/script.js
@@ -91,3 +91,65 @@ async function createMatch(event) {
   }
 };
 
+async function scoreboard(event) {
+  try {
+    event.preventDefault();
+    const matchName = document.getElementById("match_name").value;
+
+    const team1 = {
+      name: document.getElementById("team1_name").value,
+      players: [
+        document.getElementById("team1_player1").value,
+        document.getElementById("team1_player2").value,
+        document.getElementById("team1_player3").value,
+        document.getElementById("team1_player4").value,
+        document.getElementById("team1_player5").value,
+        document.getElementById("team1_player6").value,
+        document.getElementById("team1_player7").value,
+        document.getElementById("team1_player8").value,
+        document.getElementById("team1_player9").value,
+        document.getElementById("team1_player10").value,
+        document.getElementById("team1_player11").value
+      ]
+    };
+
+    const team2 = {
+      name: document.getElementById("team2_name").value,
+      players: [
+        document.getElementById("team2_player1").value,
+        document.getElementById("team2_player2").value,
+        document.getElementById("team2_player3").value,
+        document.getElementById("team2_player4").value,
+        document.getElementById("team2_player5").value,
+        document.getElementById("team2_player6").value,
+        document.getElementById("team2_player7").value,
+        document.getElementById("team2_player8").value,
+        document.getElementById("team2_player9").value,
+        document.getElementById("team2_player10").value,
+        document.getElementById("team2_player11").value
+      ]
+    };
+    const duration = document.getElementById("duration").value;
+
+    let data = {
+      name: matchName,
+      duration: duration,
+      team1: team1,
+      team2: team2
+    };
+
+    data = JSON.stringify(data);
+
+    let response = await fetch("/matches/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: data,
+    });
+    response = await response.json();
+    window.location.href = '/';
+  } catch (error) {
+    console.log(error);
+  }
+};
--- routes/matches.js
+++ routes/matches.js
@@ -8,7 +8,8 @@ router.get("/scheduleMatch", matches.getScheduleMatch);
 router.post("/", matches.createMatch);
 router.get("/history", matches.getMatches);
 router.get("/viewMatch", matches.viewMatch);
-
+router.get("/scorecard", matches.getScorecard);
+router.post("/scoreboard",matches.Scorecard);
 // router.put("/", matches.updateMatch);
 // router.post("/comments", matches.postComment);
 // router.post("/highlights", matches.postHighlights);
