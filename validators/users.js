f78588afb5c4c144fa5a511ec588bcf2336aa24a
Naresh-ctrl <keasyy999@gmail.com>
2022-12-18 20:09:32 -0500

    Password regex for validation

--- validators/users.js
+++ validators/users.js
@@ -1,4 +1,5 @@
 const Joi = require("joi");
+const passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
 
 module.exports = {
   validateUserSignUp,
@@ -16,7 +17,7 @@ function validateUserSignUp(requestBody) {
     firstName: Joi.string().alphanum().min(3).max(20).required(),
     lastName: Joi.string().alphanum().min(1).max(20).required(),
     username: Joi.string().alphanum().min(3).max(20).required(),
-    password: Joi.string().required(),
+    password: Joi.string().regex(passwordRegex).required(),
   });
   return schema.validate(requestBody);
 }
@@ -24,7 +25,7 @@ function validateUserSignUp(requestBody) {
 function validateUserLogin(Username, password) {
   const schema = Joi.object().keys({
     username: Joi.string().alphanum().min(3).max(20).required(),
-    password: Joi.string().required(),
+    password: Joi.string().regex(passwordRegex).required(),
   });
   return schema.validate(Username, password);
 }
